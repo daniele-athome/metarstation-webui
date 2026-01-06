236189c9aeea216bc8f7ff82fb5016cb49140c12
Daniele Ricci <daniele@casaricci.it>
2026-01-06 15:52:58 +0100

    Wind chart improvements

--- scripts.js
+++ scripts.js
@@ -490,7 +490,7 @@ const weatherManager = {
                 minPadding: 0,
                 maxPadding: 0,
                 min: 0,
-                softMax: 40,
+                softMax: 30,
                 title: false,
             },
             tooltip: {
